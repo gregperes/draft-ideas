2a46683c18b24e69b1d11151ae9a29439a2e0fbd
Gregory Peres <gregory@Gregs-MacBook-Air.local>
2013-06-28 22:08:03 -0300

    Fix deploy openshift file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,1 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create(name: "Gregory Peres", email: "gregperes84@gmail", login: "gregperes", password: "123456", password_confirmation: "123456")
+User.create(name: "Gregory Peres", email: "gregperes84@gmail", login: "gregperes", password: "123456", password_confirmation: "123456") unless User.find_by_login("gregperes").present?
