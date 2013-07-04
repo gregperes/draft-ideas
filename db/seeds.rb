09d972c79cfc5a885036b2a0fbab430d4c2c73bf
Gregory Peres <gregory@Gregs-MacBook-Air.local>
2013-07-04 19:10:13 -0300

    Add Marcos user in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,2 @@
 User.create(name: "Gregory Peres", email: "gregperes84@gmail", login: "gregperes", password: "123456", password_confirmation: "123456") unless User.find_by_login("gregperes").present?
+User.create(name: "Marcos Zimermann", email: "marcoslz@hotmail.com.br", login: "marcosz", password: "123456", password_confirmation: "123456") unless User.find_by_login("marcosz").present?
