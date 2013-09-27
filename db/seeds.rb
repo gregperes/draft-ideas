dd0dbc1d8a33371375d7c8e9368bcf0e2d762b36
Gregory Peres <gregperes84@gmail.com>
2013-09-26 21:23:29 -0300

    Add wagner user to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,17 @@
-User.create(name: "Gregory Peres", email: "gregperes84@gmail", login: "gregperes", password: "123456", password_confirmation: "123456") unless User.find_by_login("gregperes").present?
-User.create(name: "Marcos Zimermann", email: "marcoslz@hotmail.com.br", login: "marcosz", password: "123456", password_confirmation: "123456") unless User.find_by_login("marcosz").present?
+User.create(name: "Gregory Peres",
+                  email: "gregperes84@gmail",
+                  login: "gregperes",
+                  password: "123456",
+                  password_confirmation: "123456") unless User.find_by_login("gregperes").present?
+
+User.create(name: "Marcos Zimermann",
+                  email: "marcoslz@hotmail.com.br",
+                  login: "marcosz",
+                  password: "123456",
+                  password_confirmation: "123456") unless User.find_by_login("marcosz").present?
+
+User.create(name: "Wagner Parmezão",
+                  email: "wagner@parnex.com.br",
+                  login: "wagner",
+                  password: "123456",
+                  password_confirmation: "123456") unless User.find_by_login("wagner").present?
