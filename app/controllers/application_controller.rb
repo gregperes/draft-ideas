29a98f68c6f12dda651baffc9ce41be423a812a8
Gregory Peres <gregperes84@gmail.com>
2013-09-28 14:38:56 -0300

    Add filter to prevent not logged users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,25 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :user_signed_in?, :current_user
 
+  before_action :require_login
+
   def store_signed_in_user(user)
-  	session[:user_id] = user.id
+    session[:user_id] = user.id
   end
 
   def current_user
-  	if user_signed_in?
-  		User.find(session[:user_id])
-  	end
+    if user_signed_in?
+      User.find(session[:user_id])
+    end
   end
 
   def user_signed_in?
-		session[:user_id].present?
-	end
+    session[:user_id].present?
+  end
+
+  def require_login
+    if !user_signed_in?
+      redirect_to signin_url, :alert => "Por favor efetue o login!"
+    end
+  end
 end
